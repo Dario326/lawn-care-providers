4bed68d63b01116b2a206e546dcc3b453d5432eb
Dario <d.sumlin974@gmail.com>
2016-10-20 15:33:43 -0400

    made changes

--- keys.js
+++
@@ -1,9 +0,0 @@
-keys {
-    
-    mongoUser: 'lawnCare',
-    mongoPass: 2525,
-    googleClientId: '315961699491-6c5kpi4n7rftvjgf4susk6pruibdg47d.apps.googleusercontent.com',
-    googleClientSecret: 'rHkRX5EDj_pQpoxSRmWb0ijY',
-    FACEBOOK_APP_ID: 1804669116435316,
-    FACEBOOK_APP_SECRET: '0872ab661595c1effa825d7173d36e81'
-}
